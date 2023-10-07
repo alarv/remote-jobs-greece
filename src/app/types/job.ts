c27cb19307b9c226909d8b0072610aa3d3545f52
alex.arvanitidis <alex.arvanitidis@mimacom.com>
2023-10-07 21:31:59 +0300

    feat: show job listings

---
+++ src/app/types/job.ts
@@ -0,0 +1,13 @@
+export interface Job {
+  id: number;
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+  };
+  acf: {
+    company_name: string;
+    job_type: string[];
+  };
+}
