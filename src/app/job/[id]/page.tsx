1876351b9f5baed1536f1e229bbc2492e982dda3
Iphigeneia Krousti <iphigeneiakr@gmail.com>
2023-11-03 17:41:14 +0200

    feat: first version of job page

--- src/app/job/[id]/page.tsx
+++ src/app/job/[id]/page.tsx
@@ -21,16 +21,21 @@ export default async function Page({ params }: { params: { id: string } }) {
   const markup = { __html: job.content.rendered };
 
   return (
-    <main className="mx-auto max-w-7xl px-2 py-5 sm:px-6 lg:px-8 bg-gray-100">
-      <div className="w-4/5 mx-auto p-8 my-8 bg-white shadow-md rounded-lg">
+    <main className="mx-auto max-w-7xl px-2 py-5 sm:px-6 lg:px-8 ">
+      <div className=" mx-auto py-5 rounded-lg">
+        <div className='grid grid-cols-5 gap-20 divide-x divide-slate-200 '>
+        <div className='col-span-3'>
         <h1 className="text-3xl font-semibold">{job.title.rendered}</h1>
-        <p className="text-gray-500">{job.acf.company_name}</p>
-        <p className="font-semibold mt-4">
-          {job.acf.salary_minimum_range}-{job.acf.salary_maximum_range}$
+        <p className="mt-4" dangerouslySetInnerHTML={markup}></p>
+
+        </div>
+        <div className='col-span-2 container '>
+          <div className='mx-4	'>
+        <p className=" font-semibold mt-4">Company name: {job.acf.company_name}</p>
+        <p className="font-semibold mt-4">Salary Range: {job.acf.salary_minimum_range}-{job.acf.salary_maximum_range}$
         </p>
-        <p className="font-semibold">{job.acf.employment_type}</p>
-        <p className="font-semibold">{job.acf.job_type}</p>
-        <p className="font-semibold">{job.acf.job_field}</p>
+        <p className="font-semibold">Employment type: {job.acf.employment_type}</p>
+        <p className="font-semibold">Job field: {job.acf.job_field}</p>
         <ul className="flex items-start flex-wrap mt-4">
           <li className="flex mx-1">
             <a
@@ -41,7 +46,9 @@ export default async function Page({ params }: { params: { id: string } }) {
             </a>
           </li>
         </ul>
-        <p className="mt-4" dangerouslySetInnerHTML={markup}></p>
+        </div>
+        </div>
+      </div>
       </div>
     </main>
   );
