f36724aae7e6a1ff88970716a8f161d6847f912d
alex.arvanitidis <alex.arvanitidis@mimacom.com>
2023-11-05 23:46:51 +0200

    fix: dangerouslySetInnerHTML set as div

--- src/app/job/[id]/page.tsx
+++ src/app/job/[id]/page.tsx
@@ -23,34 +23,40 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <main className="mx-auto max-w-7xl px-2 py-5 sm:px-6 lg:px-8 ">
       <div className=" mx-auto py-5 rounded-lg">
-        <div className='grid grid-cols-5 gap-20 divide-x divide-slate-200 '>
-        <div className='col-span-3'>
-        <h1 className="text-3xl font-semibold">{job.title.rendered}</h1>
-        <p className="mt-4" dangerouslySetInnerHTML={markup}></p>
-
+        <div className="grid grid-cols-5 gap-20 divide-x divide-slate-200 ">
+          <div className="col-span-3">
+            <h1 className="text-3xl font-semibold">{job.title.rendered}</h1>
+            <div className="mt-4" dangerouslySetInnerHTML={markup}></div>
+          </div>
+          <div className="col-span-2 container ">
+            <div className="mx-4	">
+              <p className=" font-semibold mt-4">
+                Company name: {job.acf.company_name}
+              </p>
+              <p className="font-semibold mt-4">
+                Salary Range: {job.acf.salary_minimum_range}-
+                {job.acf.salary_maximum_range}$
+              </p>
+              <p className="font-semibold">
+                Employment type: {job.acf.employment_type}
+              </p>
+              <p className="font-semibold">Job field: {job.acf.job_field}</p>
+              <ul className="flex items-start flex-wrap mt-4">
+                <li className="flex mx-1">
+                  {job.acf.tags.map((tag) => (
+                    <a
+                      key={tag.name}
+                      className="p-2 px-3 border-purple-800 mb-4 rounded font-medium hover:bg-transparent hover:border-purple-800 border bg-purple-100 text-purple-700 rounded-full text-indigo-500"
+                      href="category/all"
+                    >
+                      {tag.name}
+                    </a>
+                  ))}
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className='col-span-2 container '>
-          <div className='mx-4	'>
-        <p className=" font-semibold mt-4">Company name: {job.acf.company_name}</p>
-        <p className="font-semibold mt-4">Salary Range: {job.acf.salary_minimum_range}-{job.acf.salary_maximum_range}$
-        </p>
-        <p className="font-semibold">Employment type: {job.acf.employment_type}</p>
-        <p className="font-semibold">Job field: {job.acf.job_field}</p>
-        <ul className="flex items-start flex-wrap mt-4">
-          <li className="flex mx-1">
-            {job.acf.tags.map((tag) => (
-              <a
-                className="p-2 px-3 border-purple-800 mb-4 rounded font-medium hover:bg-transparent hover:border-purple-800 border bg-purple-100 text-purple-700 rounded-full text-indigo-500"
-                href="category/all"
-              >
-                {tag.name}
-              </a>
-            ))}
-          </li>
-        </ul>
-        </div>
-        </div>
-      </div>
       </div>
     </main>
   );
