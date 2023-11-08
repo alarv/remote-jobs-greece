1b97f9b4a9cc02fdd15cc2c25766429e02aef4c4
Iphigeneia Krousti <iphigeneiakr@gmail.com>
2023-11-08 09:32:18 +0200

    feat:sticky footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   const d = new Date();
   let year = d.getFullYear();
   return (
-    <footer className=" bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <footer className=" sticky top-[100vh] w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © {year + ' '}
         <a href="https://flowbite.com/" className="hover:underline">
