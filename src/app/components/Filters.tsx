dcfe5f5d2a28dce9902205ba36f98a1a73d6f7e1
Iphigeneia Krousti <iphigeneiakr@gmail.com>
2023-11-09 14:16:34 +0200

    feat:job page styling

--- src/app/components/Filters.tsx
+++ src/app/components/Filters.tsx
@@ -244,7 +244,7 @@ export function Filters(props: FiltersProps) {
           </Dialog>
         </Transition.Root>
 
-        <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <main className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
           <div className="flex items-baseline justify-between md:border-b border-gray-200 pb-6 pt-9">
             <div className="flex items-center">
               <Menu as="div" className="relative inline-block text-left">
