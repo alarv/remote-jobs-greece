cd6264fe6f17690d61b7b17acfd18adb19504d01
Iphigeneia Krousti <iphigeneiakr@gmail.com>
2023-11-13 13:12:43 +0200

    feat: jobpage styling

--- src/app/components/Job.tsx
+++ src/app/components/Job.tsx
@@ -44,8 +44,6 @@ Jobs        </h5>
           <li className="py-3 sm:py-4">
             <div className="flex items-center space-x-4">
               <div className="flex-shrink-0">
-                {/*<img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg"*/}
-                {/*     alt="Neil image"> */}
               </div>
               <div className="flex-1 min-w-0">
                 <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
