e78c4e24f5b0562c9d136cbab08c162a51530a78
alex.arvanitidis <alex.arvanitidis@mimacom.com>
2023-11-15 00:14:21 +0200

    fix: check if jobs is defined

--- src/app/components/JobListings.tsx
+++ src/app/components/JobListings.tsx
@@ -42,9 +42,7 @@ export default function JobListings(props: JobListingProps) {
           </div>
         </div>
         <div className="border shadow border-gray-200 rounded-b-lg grid grid-cols-1 divide-y divide-gray-100">
-          {jobs.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
+          {jobs && jobs.map((job) => <JobListing key={job.id} job={job} />)}
         </div>
         <div>
           <Pagination
