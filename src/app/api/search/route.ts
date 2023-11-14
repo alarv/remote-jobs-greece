dd366c9f95a943b59514b30504b59901e46f73e3
Alex Arvanitidis <alarvfm@gmail.com>
2023-11-14 17:47:38 +0200

    feat: search route (#12)

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const apiURL = process.env.API_URL!;
+  const jobQueryParam = 'subtype=job';
+
+  const searchParams = request.nextUrl.searchParams;
+  const queryString = `${searchParams.toString()}&${jobQueryParam}`;
+
+  try {
+    const url = `${apiURL}/search?${queryString}`;
+    const response = await fetch(url, {
+      cache: 'no-store',
+    });
+    const data = await response.json();
+    return Response.json(data);
+  } catch (err) {
+    console.error('route jobs could not be retrieved');
+    return Response.json([]);
+  }
+}
