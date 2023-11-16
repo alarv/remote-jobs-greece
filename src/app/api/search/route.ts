3a39900f8c6b5153617857b01bf9f75c956a9532
alex.arvanitidis <alex.arvanitidis@mimacom.com>
2023-11-16 10:29:57 +0200

    chore: test if ip retrieval works

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { isDevEnvironment } from '@/app/util/env.util';
-import rateLimit from '@/app/util/rate-limit.util';
+import rateLimit, { retrieveIp } from '@/app/util/rate-limit.util';
 
 const REQUEST_RATE_LIMIT = 10;
 const REQUEST_TIME_WINDOW_IN_MS = 60 * 1000; // 60 seconds
+const CACHE_KEY_PREFIX = 'search';
 
 const limiter = rateLimit({
   interval: REQUEST_TIME_WINDOW_IN_MS, // 10 minutes
@@ -17,10 +18,16 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const queryString = `${searchParams.toString()}&${jobQueryParam}`;
 
+  const ip = retrieveIp(request);
+  if (!ip) {
+    console.warn('request ip could not be retrieved');
+  }
+
   const { isRateLimited, limit, limitRemaining } = limiter.check(
     REQUEST_RATE_LIMIT,
-    'CACHE_TOKEN',
-  ); // 10 requests per minute
+    `${CACHE_KEY_PREFIX}_${ip}`,
+  ); // 10 requests per minute per IP
+
   if (isRateLimited) {
     return NextResponse.json(
       { message: 'Rate limited' },
--- src/app/util/rate-limit.util.ts
+++ src/app/util/rate-limit.util.ts
@@ -1,9 +1,21 @@
 import { LRUCache } from 'lru-cache';
+import { NextRequest } from 'next/server';
 
 type Options = {
   uniqueTokenPerInterval?: number;
   interval?: number;
 };
+
+export function retrieveIp(req: NextRequest): string | null {
+  let ipAddress = req.ip || req.headers.get('x-real-ip');
+
+  const forwardedFor = req.headers.get('x-forwarded-for');
+  if (!ipAddress && forwardedFor) {
+    ipAddress = forwardedFor?.split(',').at(0) ?? 'Unknown';
+  }
+
+  return ipAddress;
+}
 
 export default function rateLimit(options?: Options) {
   const tokenCache = new LRUCache({
@@ -12,10 +24,10 @@ export default function rateLimit(options?: Options) {
   });
 
   return {
-    check: (limit: number, token: string) => {
-      const tokenCount = (tokenCache.get(token) as number[]) || [0];
+    check: (limit: number, cacheKey: string) => {
+      const tokenCount = (tokenCache.get(cacheKey) as number[]) || [0];
       if (tokenCount[0] === 0) {
-        tokenCache.set(token, tokenCount);
+        tokenCache.set(cacheKey, tokenCount);
       }
       tokenCount[0] += 1;
 
